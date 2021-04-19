2c5679e51f0ec6b4ddd236b526c35ecac6a9f8c5
Lallie <lallie.audry@gmail.com>
2021-04-19 11:51:02 +0200

    feat(checking-fields): checking fields and display error messages

---
+++ starterOnly/form.js
@@ -0,0 +1,32 @@
+let formularIsValid = false;
+let date = new Date()
+
+const inputDate = document.querySelector(".formData input[type=date]")
+
+
+inputDate.setAttribute("max", date.getFullYear() - 15 + "-" + String(date.getMonth()).padStart(2, "0") + "-" + String(date.getDate()).padStart(2, "0"))
+
+function validate(event)
+{
+    event.preventDefault();
+    formularIsValid = true;
+
+    formData.forEach(element => {
+        element.querySelectorAll("input").forEach((input) =>{
+            if(!input.checkValidity()){
+                element.setAttribute('data-error-visible', true);
+                formularIsValid = false;
+            }
+            else {
+                element.setAttribute('data-error-visible', false);
+            }
+        });
+    });
+
+    if(formularIsValid){
+        modalbg.style.display = "none";
+    }
+
+}
+
+
